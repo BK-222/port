3257e7a4c1b90e056f5c9251b9bb744b917f0f89
abc <abc@example.com>
2022-04-08 20:28:43 +0100

    inor change

--- components/layout/theHeader.js
+++ components/layout/theHeader.js
@@ -2,15 +2,15 @@ const theHeader = {
 	template:
 	'<div>\
 		<nav class="flex justify-end">\
-			<ul class="flex items-center mt-1 mr-6 font-bold text-sm">\
+			<ul class="flex items-center mt-1 mr-6 font-bold text-sm overflow-x-auto">\
 				<li class="m-5 hover-text-cyan-500 delay-100">\
-					<router-link to="/about"><span class="text-cyan-500 pr-1.5">#01.</span>About</router-link>\
+					<router-link to="/about"><span class="text-cyan-500 pr-1">#01.</span>About</router-link>\
 				</li>\
 				<li class="m-5 hover-text-cyan-500 delay-100">\
-					<router-link to="/projects"><span class="text-cyan-500 pr-1.5">#02.</span>Projects</router-link>\
+					<router-link to="/projects"><span class="text-cyan-500 pr-1">#02.</span>Projects</router-link>\
 				</li>\
-				<li class="m-5 hover-text-cyan-500 delay-100">\
-					<router-link to="/dev-skills"><span class="text-cyan-500 pr-1.5">#03.</span>Dev. Skills</router-link>\
+				<li class="m-5 hover-text-cyan-500 delay-100 whitespace-nowrap">\
+					<router-link to="/dev-skills"><span class="text-cyan-500 pr-1">#03.</span>Dev. Skills</router-link>\
 				</li>\
 				<li class="m-5">\
 					<base-link to="/contact">Contact</base-link>\
